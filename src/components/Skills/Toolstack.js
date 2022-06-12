08e0454c33b419fd7dbee2233f012cc5140e6b78
Pwnkdm <pawanrajkadam@gmail.com>
2022-06-12 12:41:54 +0530

    vercel added

--- src/components/Skills/Toolstack.js
+++ src/components/Skills/Toolstack.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { SiVisualstudiocode, SiPostman, SiHeroku } from "react-icons/si";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiHeroku,
+  SiVercel,
+} from "react-icons/si";
 
 function Toolstack() {
   return (
@@ -19,6 +24,9 @@ function Toolstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiHeroku />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
       </Col>
     </Row>
   );
